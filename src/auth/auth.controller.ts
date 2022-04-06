c20df9a0d07533160e663fbf66004883ce7d71c8
Leo <lcheung@swellbottle.com>
2022-04-06 09:57:33 -0400

    remove comment

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, ParseIntPipe, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto';
 
@@ -11,7 +11,6 @@ export class AuthController {
 
   // Route - /auth/signup
   @Post('signup')
-  //signup(@Body() dto: AuthDto) {
   signup(@Body() dto: AuthDto) {
     console.log({
       dto,
