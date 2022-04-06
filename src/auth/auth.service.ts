d69b76166385863cbdea4b5f8ab84dd7ac4753ff
Leo <lcheung@swellbottle.com>
2022-04-06 10:06:41 -0400

    create user and hide hash in json response

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,30 @@
 import { Injectable } from '@nestjs/common';
 import { User, Bookmark } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
 
 @Injectable()
 export class AuthService {
   // dependency injection
   constructor(private prisma: PrismaService) {}
-  signup() {
-    // auth convert to json object
-    return { msg: 'I have signed up' };
+
+  async signup(dto: AuthDto) {
+    // generate the password hash
+    const hash = await argon.hash(dto.password);
+
+    // save the new user to db
+    const user = await this.prisma.user.create({
+      data: {
+        email: dto.email,
+        hash,
+      },
+    });
+
+    // remove hash from returning in json response
+    delete user.hash;
+
+    return user;
   }
 
   signin() {
