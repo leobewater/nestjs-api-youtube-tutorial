f74bc43c53979d60aad01e4bb1e5fb7ff6551055
Leo <lcheung@swellbottle.com>
2022-04-06 08:20:39 -0400

    set PrismaModule as global module

--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -1,6 +1,8 @@
-import { Module } from '@nestjs/common';
+import { Module, Global } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 
+// make this a global module and will be shared in app
+@Global()
 @Module({
   providers: [PrismaService],
   exports: [PrismaService],
