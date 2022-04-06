42d84dd347cb7b5c113a007f771644d1e4bfa0e6
Leo <lcheung@swellbottle.com>
2022-04-06 08:16:47 -0400

    import PrismaService into AuthService

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { User, Bookmark } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable({})
 export class AuthService {
+  // dependency injection
+  constructor(private prisma: PrismaService) {
+    
+  }
   signup() {
     // auth convert to json object
     return { msg: 'I have signed up' };
