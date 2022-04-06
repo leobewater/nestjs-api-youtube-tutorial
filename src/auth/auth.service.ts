0ba8e9f2ebed4903db3c063b04841b7f5254f3d6
Leo <lcheung@swellbottle.com>
2022-04-06 08:54:30 -0400

    remove object in injectable

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,12 +2,10 @@ import { Injectable } from '@nestjs/common';
 import { User, Bookmark } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
 
-@Injectable({})
+@Injectable()
 export class AuthService {
   // dependency injection
-  constructor(private prisma: PrismaService) {
-    
-  }
+  constructor(private prisma: PrismaService) {}
   signup() {
     // auth convert to json object
     return { msg: 'I have signed up' };
