4174b78c969763bdd5c9bd08fc93d5fce7f6db73
Leo <lcheung@swellbottle.com>
2022-04-06 09:57:23 -0400

    strip out elements not defined in dto passed from request

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      // stripping out elements not defined in dto if passed from request
+      whitelist: true,
+    }),
+  );
   await app.listen(3333);
 }
 bootstrap();
