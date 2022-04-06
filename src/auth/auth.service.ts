b43d12528baac824fe938f04f23190256db14ae6
Leo <lcheung@swellbottle.com>
2022-04-06 11:32:29 -0400

    add error handling when creating duplicated user

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { User, Bookmark } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 
 @Injectable()
 export class AuthService {
@@ -14,17 +15,28 @@ export class AuthService {
     const hash = await argon.hash(dto.password);
 
     // save the new user to db
-    const user = await this.prisma.user.create({
-      data: {
-        email: dto.email,
-        hash,
-      },
-    });
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
 
-    // remove hash from returning in json response
-    delete user.hash;
+      // remove hash from returning in json response
+      delete user.hash;
 
-    return user;
+      return user;
+    } catch (error) {
+      // check for unique email error
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials token');
+        }
+      }
+
+      throw error;
+    }
   }
 
   signin() {
