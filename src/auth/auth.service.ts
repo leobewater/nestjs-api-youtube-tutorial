a0906aaeb4df455190a5a31ebbcb4929b52910aa
Leo <lcheung@swellbottle.com>
2022-04-06 13:08:52 -0400

    add sign in logic

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -39,7 +39,27 @@ export class AuthService {
     }
   }
 
-  signin() {
-    return { msg: 'I have signed in' };
+  async signin(dto: AuthDto) {
+    // find the user by email
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+
+    // if user does not exist throw exception
+    if (!user) throw new ForbiddenException('Credentials incorrect');
+
+    // compare password
+    const pwMatches = await argon.verify(user.hash, dto.password);
+
+    // if password incorrect throw exception
+    if (!pwMatches) throw new ForbiddenException('Credentials incorrect');
+
+    // hide password hash in response
+    delete user.hash;
+
+    // send back user
+    return user;
   }
 }
