bf6ed213758016c08be73261717bfa3e6ad080a6
Leo <lcheung@swellbottle.com>
2022-04-07 07:57:25 -0400

    add edit user dto interface/class

---
+++ src/user/dto/edit-user.dto.ts
@@ -0,0 +1,15 @@
+import { IsEmail, IsOptional, IsString } from 'class-validator';
+
+class EditUserDto {
+  @IsEmail()
+  @IsOptional()
+  email?: string;
+
+  @IsString()
+  @IsOptional()
+  firstName?: string;
+
+  @IsString()
+  @IsOptional()
+  lastName?: string;
+}
