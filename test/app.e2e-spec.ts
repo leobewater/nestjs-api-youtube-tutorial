0a0c4824d57d9b2207b679d3ec157b2ce3865065
Leo <lcheung@swellbottle.com>
2022-04-07 08:14:26 -0400

    add edit user test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -3,7 +3,8 @@ import { Test } from '@nestjs/testing';
 import * as pactum from 'pactum';
 import { PrismaService } from '../src/prisma/prisma.service';
 import { AppModule } from '../src/app.module';
-import { AuthDto } from 'src/auth/dto';
+import { AuthDto } from '../src/auth/dto';
+import { EditUserDto } from '../src/user/dto';
 
 describe('App e2e', () => {
   let app: INestApplication;
@@ -130,7 +131,24 @@ describe('App e2e', () => {
       });
     });
 
-    describe('Edit user', () => {});
+    describe('Edit user', () => {
+      it('should edit user', () => {
+        const dto: EditUserDto = {
+          firstName: 'userABC',
+          email: 'userabc@example.com',
+        };
+
+        return pactum
+          .spec()
+          .patch('/users')
+          .withHeaders({ Authorization: 'Bearer $S{userAt}' })
+          .withBody(dto)
+          .expectStatus(200)
+          .expectBodyContains(dto.firstName)
+          .expectBodyContains(dto.email);
+        // .inspect();
+      });
+    });
   });
 
   describe('Bookmarks', () => {
